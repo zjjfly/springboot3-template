e9b79f59207b882621b322ed57364c430ac497c5
zjjfly <zjjblue@126.com>
2023-09-01 22:40:55 +0800

    update

--- src/main/java/com/github/zjjfly/template/config/OpenApiConfig.java
+++ src/main/java/com/github/zjjfly/template/config/OpenApiConfig.java
@@ -1,5 +1,6 @@
 package com.github.zjjfly.template.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -11,18 +12,19 @@ import org.springframework.context.annotation.Configuration;
  * @author Zi JunJie(junjie.zi@siemens.com)
  */
 @Configuration
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "XXX APIs", description = "some description"))
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-            .info(new Info().title("XXX APIs")
-                .description("some description")
-                .version("v1.0")
-                .license(new License().name("Apache 2.0")
-                    .url("https://www.apache.org/licenses/LICENSE-2.0.txt")))
-            .externalDocs(
-                new ExternalDocumentation().description("GitHub").url("https://github.com/zjjfly"));
+                .info(new Info().title("XXX APIs")
+                        .description("some description")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.txt")))
+                .externalDocs(
+                        new ExternalDocumentation().description("GitHub").url("https://github.com/zjjfly"));
     }
 
 }
