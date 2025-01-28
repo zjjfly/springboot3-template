b569ba496ac2bb6185c587f6ad0f1a43eb06ecef
zjjfly <zjjblue@126.com>
2025-01-28 19:09:36 +0800

    some fix

--- src/main/java/com/github/zjjfly/template/controller/PersonController.java
+++ src/main/java/com/github/zjjfly/template/controller/PersonController.java
@@ -4,13 +4,16 @@ import com.github.zjjfly.template.dto.Response;
 import com.github.zjjfly.template.model.Person;
 import com.github.zjjfly.template.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author <a href="https://github.com/zjjfly"/>zjjfly</a>
@@ -25,7 +28,7 @@ public class PersonController {
     @GetMapping
     public Response<List<Person>> search() {
         List<Person> all = personRepository.findAll(Map.of("name_eq", "foo",
-                        "age_gte", "18"), Person.class);
+                        "age_lte", "18"), Person.class);
         return Response.success(all);
     }
 
@@ -33,6 +36,12 @@ public class PersonController {
     public Response<Person> save() {
         Person person = new Person("foo",19);
         return Response.success(personRepository.save(person));
+    }
+
+    @DeleteMapping("/{id}")
+    public Response<Void> deleteById(@PathVariable UUID id) {
+        personRepository.deleteById(id);
+        return Response.success();
     }
 
 
--- src/main/java/com/github/zjjfly/template/repository/PersonRepository.java
+++ src/main/java/com/github/zjjfly/template/repository/PersonRepository.java
@@ -4,8 +4,10 @@ import app.tozzi.repository.JPASearchRepository;
 import com.github.zjjfly.template.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 /**
  * @author <a href="https://github.com/zjjfly"/>zjjfly</a>
  */
-public interface PersonRepository extends JpaRepository<Person, Long>, JPASearchRepository<Person> {
+public interface PersonRepository extends JpaRepository<Person, UUID>, JPASearchRepository<Person> {
 }
